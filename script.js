25e3fa0c826923eb8f45bd344ef247ddc8216167
Anurag Shaw <anuragshaw.as@gmail.com>
2023-11-13 14:28:40 +0530

    ecommerce website commit 1

---
+++ script.js
@@ -0,0 +1,16 @@
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+const close = document.getElementById('close');
+
+if(bar){
+    bar.addEventListener('click', ()=>{
+        console.log("clicked");
+        nav.classList.add('active');
+    })
+}
+if(close){
+    close.addEventListener('click', ()=>{
+        console.log("clicked");
+        nav.classList.remove('active');
+    })
+}
